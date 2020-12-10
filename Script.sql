58604ebecb32d9f31132bbb637a08b668781db8a
Rafael Sagawe Favero <sagawe_r@hotmail.com>
2020-12-09 22:14:20 -0300

    Alteração do nome das imagens do grafana

---
+++ Script.sql
@@ -0,0 +1,14 @@
+CREATE table if not exists contatos(
+iDCont INTEGER primary key not null,
+nome varchar(50) not null,
+fone varchar (15) not null,
+email varchar(50)
+);
+
+INSERT  into contatos (nome, fone, email)
+values 
+('Alpha', '123', 'alpha@sa.com'),
+('Betha', '456', 'betha@sa.com'),
+('Sigma', '789', 'sigma@sa.com');
+
+DROP table contatos2;
