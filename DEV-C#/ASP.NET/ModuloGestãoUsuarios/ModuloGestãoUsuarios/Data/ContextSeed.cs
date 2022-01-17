a1dc975a47c68ecf63380f7690a58e1a5505f194
rafaelsagawe <sagawe_r@hotmail.com>
2022-01-16 21:10:58 -0300

    Criação das regras padrão

---
+++ DEV-C#/ASP.NET/ModuloGestãoUsuarios/ModuloGestãoUsuarios/Data/ContextSeed.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ModuloGestãoUsuarios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuloGestãoUsuarios.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRegrasAsync (UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Semear Regras
+            await roleManager.CreateAsync(new IdentityRole(Enums.Regras.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Regras.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Regras.Moderador.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Regras.Basico.ToString()));
+        }
+    }
+}
